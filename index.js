f87a647449f61532871215fe85fe000bd63800b1
refiqi <refyfadhila96@gmail.com>
2019-03-07 18:53:28 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+// Body Parser 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/kurs', { useNewUrlParser: true }, err => {
+    if (err) throw err;
+    console.log('Database Connected');
+});
+
+
+
+
+// Setting up Server
+const port = process.env.PORT || 7000;
+app.listen(port, () => {
+    console.log(`Server Connected at ${port}`);
+})
